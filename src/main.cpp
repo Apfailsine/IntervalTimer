0829d5454256ec789ff513bcd00afcc3e4c6e61a
Apfailsine <erik.vautrin1996@gmail.com>
2025-12-10 11:15:05 +0100

    Hello World

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+#include <Wire.h>
 
 // put function declarations here:
 int myFunction(int, int);
 
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    display.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    display.clearBuffer();                 // Puffer löschen
+    display.setFont(u8g2_font_ncenB08_tr); // Schriftart setzen
+    display.drawStr(0, 10, "Hello World!"); // Text zeichnen
+    display.sendBuffer();                  // Pufferinhalt anzeigen
+    delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
